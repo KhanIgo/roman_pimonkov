bce57c181d8a94156818c7b1e5a4b2b57371cbd0
Igor' Khan <igor.swit4@gmail.com>
2019-04-12 18:19:19 +1000

    5

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,12 @@
 jQuery($ => {
     $('body').on('click', '.js-hamburger', function () {
+
+//        alert();
+
         let $this = $(this);
         $this.toggleClass('is-active');
         $('.primary-nav').toggleClass('is-active');
-    })
+    });
+
+    // VK.Widgets.ContactUs("vk_contact_us", {}, -157325072);
 });
